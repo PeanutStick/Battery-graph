0ccfdcf59075486808d7308eced1fe22430af195
PeanutStick <38986405+PeanutStick@users.noreply.github.com>
2021-09-13 19:04:58 +0000

    Update change-csv.sh

--- change-csv.sh
+++ change-csv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mv /home/peanutstick/Documents/scripts/python/bat-stat/bat-data2.csv /home/peanutstick/Documents/scripts/python/bat-stat/bat-data3.csv
-mv /home/peanutstick/Documents/scripts/python/bat-stat/bat-data1.csv /home/peanutstick/Documents/scripts/python/bat-stat/bat-data2.csv
+mv /path/to/the/same/directory/as/main.py/bat-data2.csv /path/to/the/same/directory/as/main.py/bat-data3.csv #change it 
+mv /path/to/the/same/directory/as/main.py/bat-data1.csv /path/to/the/same/directory/as/main.py/bat-data2.csv #change it
 
